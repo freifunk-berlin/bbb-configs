3f36ad1b028961640c7c99c3fbd987c6d383cbb2
Simon Polack <spolack+git@mailbox.org>
2022-03-22 19:15:06 +0100

    prepare prometheus poc

---
+++ roles/cfg_openwrt/files/ap/prom-catalog-register.sh
@@ -0,0 +1,1 @@
+../common/prom-catalog-register.sh
---
+++ roles/cfg_openwrt/files/common/prom-catalog-register.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+log() {
+    local msg="$1"
+    logger -t prom-catalog-register -s "$msg"
+    # for debugging on local machine
+    #echo "$msg"
+}
+
+dec2hex() {
+    local dec="$1"
+    printf "%X" "$dec"
+}
+
+
+PORTS="9100"
+URL="https://prometheus-catalog.freifunk.ninja/register"
+hostname="$(cat /proc/sys/kernel/hostname)"
+role="$1"
+location="$2"
+
+log "started. Registering with following labels: hostname: $hostname, role: $role, location: $location"
+
+log "Retrieving IPv6 Address"
+
+ownip="$(wget -qO /dev/stdout https://ipv6.icanhazip.com)"
+
+if [ $? -ne 0 ]; then
+  log "Error while retrieving IPv6 Address - Check Connectivity?!"
+  exit
+fi
+
+if [[ "$ownip" != "2001:bf7:*" ]]; then
+  log "Public IPv6 is not part of freifunk berlin prefix"
+  exit
+fi
+
+for _port in $PORTS; do
+  # Todo: Check if something is listning on the port (/proc/net/tcp6)
+  log "Registering [$ownip]:$_port with hostname $hostname"
+  curl -d"{\"labels\":{\"hostname\":\"$hostname\",\"role\":\"$role\",\"location\":\"$location\"},\"targets\":[\"[$ownip]:$_port\"],\"hostname\":\"$hostname\"}" \
+      -H'Content-Type: application/json' \
+      "$URL"
+  if [ $? -eq 0 ]; then    
+    log "..success"
+  else                                 
+    log "Error while registering"
+  exit                                                  
+fi  
+done
